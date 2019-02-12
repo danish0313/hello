63d5399958a01949c1829b9dbd6eec1de9afd819
danish0313 <danishoo.dk@gmail.com>
2019-02-12 17:41:15 +0500

    Updates

---
+++ precache-manifest.f51eaa701cfe3406e80b7d7122a999a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9637c00df7c99e23105d",
+    "url": "/static/js/main.9637c00d.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "9637c00df7c99e23105d",
+    "url": "/static/css/main.01034658.chunk.css"
+  },
+  {
+    "revision": "acb35816b9fb692e95c4603beeaa5674",
+    "url": "/index.html"
+  }
+];
